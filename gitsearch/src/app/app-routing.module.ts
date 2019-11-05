b6a19e6e43a9a39d3ae1b5d04d2e02cf740b641a
adriankiprono <tuimuradrian6@gmail.com>
2019-11-05 05:21:07 +0300

    add routelink for about component

--- gitsearch/src/app/app-routing.module.ts
+++ gitsearch/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {DispalyinfoComponent} from './dispalyinfo/dispalyinfo.component'
+import {AboutComponent} from './about/about.component'
 
 
 
 const routes: Routes = [
   {path:'displayinfo',component:DispalyinfoComponent},
+  {path:'about',component:AboutComponent},
 ];
 
 @NgModule({
